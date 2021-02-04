b42afcb7000e7aa98e4ac86eb8ae0a130722c334
Sterling Ortiz <sortizp@bu.edu>
2021-02-03 23:02:32 -0500

    mix ecto.create and first calls to api amde status: ok

--- library_api/config/dev.exs
+++ library_api/config/dev.exs
@@ -16,7 +16,7 @@ config :library_api, LibraryApi.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :library_api, LibraryApiWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 3000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
